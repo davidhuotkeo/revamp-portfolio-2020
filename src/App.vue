909e237fffd800d20d7da8b46e797ef80eb6c5de
David <devit.huotkeo@nbc.org.kh>
2021-03-14 20:56:57 +0700

    new website

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,220 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div id="responsive" ref="responsive">
+            <div class="resp-wrapper">
+                <p>Responsive</p>
+                <p>Devit Portfolio</p>
+                <p>Coming Soon</p>
+            </div>
+        </div>
+        <div id="wrapper" ref="contentWrapper">
+            <nav>
+                <a @click="navigateTo('wrapper')">devit huotkeo</a>
+                <a @click="navigateTo('experience')">about</a>
+                <a @click="navigateTo('portfolio')">work</a>
+                <a @click="navigateTo('contact')">contact</a>
+            </nav>
+            <section class="head" id="head">
+                <img class="logo" src="@/assets/logo.png" alt="logo" />
+                <span>Impossibly</span>
+                <span>Creative</span>
+                <span>Designer</span>
+                <span>Craft your</span>
+                <span>Brand and</span>
+                <span>Interface</span>
+                <span>To the next</span>
+                <span>Level</span>
+            </section>
+            <section id="experience">
+                <div class="left">
+                    <p class="headline">About</p>
+                    <p>
+                        I am into designing interface and user experience and
+                        branding identity. Therefore, my work has put all heart
+                        and soul into it.
+                    </p>
+                </div>
+                <div class="right">
+                    <div
+                        v-for="(exp, id) in experiences"
+                        :key="id"
+                        class="exp-detail"
+                    >
+                        <p>{{ exp.year }}</p>
+                        <p>{{ exp.title }}</p>
+                        <p>{{ exp.description }}</p>
+                    </div>
+                </div>
+            </section>
+            <section id="expert">
+                <p class="headline">Expertise</p>
+                <div class="expertise">
+                    <p>Branding Identity</p>
+                    <p>UI UX Design</p>
+                    <p>Web Development</p>
+                    <p>Software Development</p>
+                </div>
+            </section>
+            <section id="portfolio">
+                <p class="headline">My Work</p>
+                <div class="table-content">
+                    <p class="head">Project List</p>
+                    <div class="projects">
+                        <a
+                            v-for="(project, id) in work"
+                            :key="id"
+                            @click="navigateTo(project.name)"
+                            >{{ project.name }}</a
+                        >
+                    </div>
+                </div>
+                <div class="work" v-for="(project, id) in work" :key="id">
+                    <dave-portfolio :project="project"></dave-portfolio>
+                </div>
+            </section>
+            <section id="contact">
+                <p class="headline">Contact</p>
+                <div class="contact-me">
+                    <a href="tel:092651335">Telephone I</a>
+                    <a href="tel:087575857">Telephone II</a>
+                    <a href="mailto:david.huotkeo@gmail.com">Email</a>
+                    <a href="https://www.behance.net/devithuotkeo">Behance</a>
+                </div>
+            </section>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import db from "@/assets/database/data.json";
+import Portfolio from "./components/Portfolio";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            experiences: db.experience,
+            work: db.work,
+        };
+    },
+    name: "App",
+    mounted() {
+        this.resizeScreeen();
+        window.addEventListener("scroll", this.scrollChangeTheme);
+        window.addEventListener("resize", this.resizeScreeen);
+    },
+    methods: {
+        scrollChangeTheme() {
+            const head = document
+                .getElementById("head")
+                .getBoundingClientRect();
+            if (head.height + head.y < 900) {
+                document.body.classList.add("white");
+            } else {
+                document.body.classList.remove("white");
+            }
+        },
+        resizeScreeen() {
+            const width = window.innerWidth;
+            if (width < 1000) {
+                if (width < 1000 && (width >= 1200 || width <= 800)) {
+                    this.$refs.responsive.style.display = "flex";
+                    this.$refs.contentWrapper.style.display = "none";
+                    document.body.style.overflow = "hidden";
+                }
+            } else {
+                if (width > 1000 && width < 1200) {
+                    this.$refs.responsive.style.display = "none";
+                    this.$refs.contentWrapper.style.display = "block";
+                    document.body.style.overflow = "scroll";
+                }
+            }
+        },
+        navigateTo(idName) {
+            const offsetTop = document.getElementById(idName).offsetTop;
+
+            scroll({
+                top: offsetTop,
+                behavior: "smooth",
+            });
+        },
+    },
+    components: {
+        "dave-portfolio": Portfolio,
+    },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700&display=swap");
+@import "../src/assets/css/root.css";
+@import "../src/assets/css/head.css";
+@import "../src/assets/css/experience.css";
+@import "../src/assets/css/responsive.css";
+@import "../src/assets/css/contact.css";
+
+body {
+    --text-color: var(--white-color);
+    --background-color: var(--black-color);
+    margin: 0;
+    color: var(--text-color);
+    background-color: var(--background-color);
+    transition: background 1.5s cubic-bezier(0.075, 0.82, 0.165, 1),
+        color 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Nunito Sans", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: start;
+    min-height: 300vh;
+    margin: 0 12vw;
+}
+
+section {
+    margin-bottom: 150px;
+}
+
+.headline {
+    font-size: 3rem;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+}
+
+.black {
+    --text-color: var(--white-color);
+    --background-color: var(--black-color);
+}
+
+.white {
+    --text-color: var(--black-color);
+    --background-color: var(--white-color);
+}
+
+.table-content {
+    margin-bottom: 50px;
+}
+
+p.head {
+    font-size: 1.5rem;
+}
+
+.projects {
+    display: flex;
+    flex-direction: column;
+}
+
+.projects > a {
+    cursor: pointer;
+    opacity: 0.5;
+    font-style: italic;
+    width: fit-content;
+    margin-bottom: 5px;
+}
+
+.projects > a:hover {
+    opacity: 1;
+    font-style: normal;
 }
 </style>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="portfolio" :id="project.name">
+        <img
+            class="image-cover"
+            :src="getImage(project.image)"
+            :alt="project.name"
+        />
+        <p class="heading">{{ project.name }}</p>
+        <p class="text">{{ project.description }}</p>
+        <div class="content">
+            <div
+                v-for="(content, id) in project.content"
+                :key="id"
+                class="content-wrap"
+            >
+                <img
+                    v-if="content.type == 'image'"
+                    :src="getImage(content.content)"
+                />
+                <p class="text" v-if="content.type == 'text'">
+                    {{ content.content }}
+                </p>
+            </div>
+        </div>
+        <div class="more" ref="more">
+            <div class="more-content" v-for="(content, id) in project.more" :key="id">
+                <img
+                    v-if="content.type == 'image'"
+                    :src="getImage(content.content)"
+                />
+                <p class="title" v-if="content.type == 'title'">
+                    {{ content.content }}
+                </p>
+                <p class="text" v-if="content.type == 'text'">
+                    {{ content.content }}
+                </p>
+            </div>
+        </div>
+        <p v-if="toggled" @click="moreLess" class="read">Roll Back</p>
+        <p v-else @click="moreLess" class="read">Read More</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["project"],
+    data() {
+        return {
+            toggled: false,
+        };
+    },
+    methods: {
+        getImage(filename) {
+            return require(`@/assets/image/${filename}`);
+        },
+        moreLess() {
+            let moreStyle = this.$refs.more.style
+            let heightContent = this.$refs.more.scrollHeight
+
+            if (this.toggled) {
+                this.navigateTo(this.project.name)
+                moreStyle.height = "0px"
+            } else moreStyle.height = `${heightContent}px`
+
+            this.toggled = !this.toggled;
+        },
+        navigateTo(idName) {
+            const offsetTop = document.getElementById(idName).offsetTop;
+
+            scroll({
+                top: offsetTop,
+                behavior: "smooth",
+            });
+        },
+    },
+};
+</script>
+
+<style scoped>
+@import "../../src/assets/css/portfolio.css";
+</style>
